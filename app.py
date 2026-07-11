caf521a630bf7cf59e1305a9a3ba701472d34739
giamsk <xrc3110@gmail.com>
2026-07-11 15:02:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ user_id = st.text_input("이름를 입력하세요", placeholder="example_user")
 ai_model = st.radio("학년", ["1", "2", "3"], horizontal=True)
 tone = st.selectbox("반", ["1", "2", "3"])
 creativity = st.slider("난이도", 0, 100, 50)
-ai_speed = st.select_slider("난이도",options=["하", "중", 상",],value="중")
+ai_speed = st.select_slider("난이도",options=["하", "중", "상"],value="중")
 
 question = st.text_area("소감", placeholder="여기에 작성해 주세요.")
 if st.button("확인"):
